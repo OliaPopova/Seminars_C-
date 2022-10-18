69458dd282b11400b6a82492a3b843b795154c8a
Olga Popova <olia.popova2001@mail.ru>
2022-10-18 19:40:46 +0300

    решение 10 задачи

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+Console.WriteLine("Задача #10 ");
+Console.Write("Введи трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string string_a = Convert.ToString(a);
+Console.WriteLine("Ответ:"+string_a[1]);
