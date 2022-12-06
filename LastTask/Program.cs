6752cd86f0628e209d67aeb2d573e13ebb142f19
Olga Popova <olia.popova2001@mail.ru>
2022-12-06 21:53:26 +0300

    готовое решение проекта

--- LastTask/Program.cs
+++ LastTask/Program.cs
@@ -1,20 +1,38 @@
 ﻿//Финальное задание курса
 
 string[] myArray1 = new string[6] {"hello","!","67543", "world", "22", "foo"};
-string[] myArray2 = new string[myArray1.Length];
 
-
-void ElementsSelection(string[] myArray1, string[] myArray2){
+string[] ElementsSelection(string[] myArray1){
     int count = 0;
     for (int i = 0; i < myArray1.Length; i++)
     {
     if(myArray1[i].Length <= 3)
         {
-        myArray2[count] = myArray1[i];
         count++;
         }
     }
+    string[] myArray2 = new string[count];
+    int j = 0;
+    for (int i = 0; i < myArray1.Length; i++)
+    {
+    if(myArray1[i].Length <= 3)
+        {
+        myArray2[j] = myArray1[i];
+        j++;
+        }
+    }
+    return myArray2;
 }
 
-void PrintArray(){}
-
+void PrintArray(string[] array){
+    Console.Write("[ ");
+     for (int i = 0; i < array.Length; i++)
+    {   
+        Console.Write($"\"{array[i]}\" ");
+    }
+    Console.Write(" ]");
+}
+string[] myArrayFinal=ElementsSelection(myArray1);
+PrintArray(myArray1);
+ Console.Write($"-> ");
+PrintArray(myArrayFinal);
