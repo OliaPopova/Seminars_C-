3c4edbb81632d9cdd128a0fb27c71c594f33a37a
Olga Popova <olia.popova2001@mail.ru>
2022-12-06 20:22:35 +0300

    скелет будущей программы

---
+++ LastTask/Program.cs
@@ -0,0 +1,9 @@
+﻿//Финальное задание курса
+
+string[] myArray1 = new string[6] {"hello","!","67543", "world", "22", "foo"};
+string[] myArray2 = new string[myArray1.Length];
+
+
+void ElementsSelection(string[] myArray1, string[] myArray2){}
+void PrintArray(){}
+
